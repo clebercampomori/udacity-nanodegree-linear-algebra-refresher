5822dcd2390c37c1fa53eb75c1398355841ff871
Cleber Campomori <cleber.campomori@treinaweb.com.br>
2018-03-24 02:23:50 -0300

    Adding the functions magnitude() and normalize()

--- Main.py
+++ Main.py
@@ -4,19 +4,29 @@ class Main:
 
 
     if __name__ == "__main__":
-        vet1 = Vector([1.33, 2.44, 3.55])        
-        vet2 = Vector([4.66, 5.77, -1.22])
-        vet3 = Vector([1.33, 2.44, 3.55])
+        # vet1 = Vector([1.33, 2.44, 3.55])        
+        # vet2 = Vector([4.66, 5.77, -1.22])
+        # vet3 = Vector([1.33, 2.44, 3.55])
 
-        print(vet1)
-        print(vet1 == vet2)
-        print(vet1 == vet3)
+        # print(vet1)
+        # print(vet1 == vet2)
+        # print(vet1 == vet3)
 
-        vetAdd = vet1.add(vet2)
-        vetSub = vet1.subtract(vet2)
-        vetMult = vet1.scalar_multiply(vet2)
+        # vetAdd = vet1.add(vet2)
+        # vetSub = vet1.subtract(vet2)
+        # vetMult = vet1.scalar_multiply(vet2)
 
-        print(' === Add, Sub and Scalar Mult ===')
-        print(vetAdd)
-        print(vetSub)
-        print(vetMult)
+        # print(' === Add, Sub and Scalar Mult ===')
+        # print(vetAdd)
+        # print(vetSub)
+        # print(vetMult)
+
+        print(' === Magnitude and normalization ===')
+        vetM1 = Vector([-0.221, 7.437])
+        print('Magnitude of {} = {}'.format(vetM1, vetM1.magnitude()))
+        vetM2 = Vector([8.813, -1.331, -6.247])
+        print('Magnitude of {} = {}'.format(vetM2, vetM2.magnitude()))
+        vetN1 = Vector([5.581, -2.136])
+        print('Normalization of {} = {}'.format(vetN1, vetN1.normalize()))
+        vetN2 = Vector([1.996, 3.108, -4.554])
+        print('Normalization of {} = {}'.format(vetN2, vetN2.normalize()))
--- Vector.py
+++ Vector.py
@@ -1,5 +1,5 @@
 import operator
-
+import math
 
 class Vector(object):
 
@@ -21,7 +21,13 @@ class Vector(object):
         return Vector(tuple(map(operator.sub, self.coordinates, otherVector.coordinates)))
 
     def scalar_multiply(self, factor):
-        return Vector([v * factor for v in self.coordinates])
+        return Vector([round(v * factor, 3) for v in self.coordinates])
+
+    def magnitude(self):
+        return round(math.sqrt(sum(round(p * p, 3) for p in self.coordinates)), 3)
+
+    def normalize(self):
+        return self.scalar_multiply(1/self.magnitude())
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
